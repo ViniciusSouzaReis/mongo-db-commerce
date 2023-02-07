360ee53c615a45393effbe835ed47b7ba5ee11fc
Vinicius Luiz de Souza Reis <viniciusluizdesouzareis@pop-os.localdomain>
2023-02-07 16:23:26 -0300

    Requisito 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,8 @@
+db.produtos.updateOne(
+  { nome: { $eq: "Big Mac" } },
+  { $currentDate: {
+    ultimaModificacao: { $type: "timestamp" },
+  } },
+);
+
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
